1cd0e1735341de5c10d1677b2c1a717239bf4cae
Ash Hellwig <ahellwig.dev@gmail.com>
2020-05-26 00:27:18 -0600

    finish script

--- get_directory_size.sh
+++ get_directory_size.sh
@@ -13,8 +13,13 @@
 # -K Output in KB
 #
 
-function get_size_string() {
+set +o errexit
+set +o nounset
+set +o pipefail
+
+function __get_size_string() {
   declare -g -x dir_size_string
+
   dir_size_string=$(du \
     -l \
     --exclude "**/node_modules/**" \
@@ -27,16 +32,104 @@ function get_size_string() {
     | awk '{ print \$1 }' \
     | sed 's/M/000/g' \
     | sed 's/K/00/g')
-
-  return dir_size_string
 }
 
-function get_size_array() {
+function __get_size_array() {
   declare -g -x dir_size_array
 
-  get_size_string | while read line; do
+  __get_size_string "$1" | while read -r line; do
     dir_size_array+="$line"
   done
 }
+
+function main() {
+  declare -g -x use_gb
+  use_gb=false
+  declare -g -x use_mb
+  use_mb=false
+  declare -g -x use_kb
+  use_kb=false
+  declare -g -x dir_to_use
+  dir_to_use=""
+
+  while getopts ":hGMKd:" arg; do
+    case "${arg}" in
+      G)
+        use_gb=false
+        use_mb=false
+        use_kb=false
+        shift
+        ;;
+      M)
+        use_gb=false
+        use_mb=true
+        use_kb=false
+        shift
+        ;;
+      K)
+        use_gb=false
+        use_mb=false
+        use_kb=true
+        shift
+        ;;
+      d)
+        dir_to_use="${OPTARG}"
+        shift
+        ;;
+      h)
+        echo -e "\033[1;31mYou need to specify [-G|-M|-K] -d [DIR]\033[0m"
+        exit 0
+        ;;
+      ?)
+        echo -e "\033[1;31mYou need to specify [-G|-M|-K] -d [DIR]\033[0m"
+        exit 1
+        ;;
+      *)
+        echo -e "\033[1;31mYou need to specify [-G|-M|-K] -d [DIR]\033[0m"
+        exit 1
+        ;;
+    esac
+  done
+
+  if [[ "$use_gb" -eq 0 ]]; then
+
+    declare -g -x DIR_SIZE_ARRAY
+    DIR_SIZE_ARRAY="$(__get_size_array "${dir_to_use}")"
+
+    declare -g -x gb_size
+
+    for i in ${DIR_SIZE_ARRAY[*]}; do
+      declare -g total_sum
+      total_sum=$((total_sum + i))
+    done
+
+    gb_size=$((total_sum / 1000))
+
+    echo "${gb_size}GB"
+  fi
+}
+
+main
+
+# --- Clean ---
+# Global Variables
+unset dir_size_string
+unset dir_size_array
+unset use_gb
+unset use_mb
+unset use_kb
+unset dir_to_use
+unset total_sum
+unset DIR_SIZE_ARRAY
+
+# Functions
+unset __get_size_string
+unset __get_size_array
+unset main
+
+# Options
+set -o errexit
+set -o nounset
+set -o pipefail
 
 # vim: set et ts=2 sw=2 ft=bash:
